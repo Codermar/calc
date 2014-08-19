b82ec41b18efa378912c24ecf77e4dfc9ac04b91
Jose Alfonso <jgalfonso@me.com>
2014-08-18 17:56:42 -0700

    Minor screen font resizing.

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ A simple browser calculator with a UI design emulating the iPhone 5 (OSx 7) calc
 
 ## Features
 
-* Addition, subtraction, multiplication and division
-* Chaining of results
+* Addition, subtraction, multiplication and division.
+* Chaining of results.
 * Clearing results for all input and for the right operand.
 * Self operating numbers. i.e press 5 x then = and you get the result of 5x5. 
 * Continued processing: Pressing the = key after entering the right operand will continue processing the result indefinitely.
-
+* Screen font resizing based on the result length.
 
 ## Installation
 
--- calc.js
+++ calc.js
@@ -20,23 +20,45 @@
 		}
 	};
 
+	var resizeScreenFont = function(len) {
+
+		var fontSize;
+
+		if(len > 9 && len <= 12) {
+			fontSize = '40px';
+		} else if(len > 12 && len <= 25) {
+			fontSize = '20px';
+		} else if (len > 25 && len < 35) {
+			fontSize = '15px';
+		} else if (len > 25) {
+			fontSize = '13px';
+			screen.style.overflowY = 'scroll';
+		} else {
+			fontSize = '60px';
+			screen.style.overflowY = 'hidden';
+		}
+		screen.style.fontSize = fontSize;
+	};
+	
 	// install event handlers
 	for (var idx = 0; idx < keys.length; idx += 1) {
 		keys[idx].onclick = function (e) {
 			var me = this;
 			return (function () {
 
-				var mem, input, isOperator;
+				var mem, input, isOperator,screenValue;
 				input = me.innerHTML;
 
 				calc.processInput(input);
 				mem = calc.getMem();
-				isOperator = mem.operators.indexOf(input) !== -1;
+				isOperator = mem.operators.indexOf(input) !== -1,
+				screenValue = calc.getScreenValue();
 
 				highlightOperator(isOperator ? input : null);
 
 				clearBtn.innerHTML = mem.clear;
-				screen.innerHTML = calc.getScreenValue();
+				resizeScreenFont(screenValue.toString().length);
+				screen.innerHTML = screenValue;
 
 			})();
 		}
